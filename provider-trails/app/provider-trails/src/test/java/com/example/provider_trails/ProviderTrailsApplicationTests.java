1176b63581c449de96a55a10cbd549361fc62482
itsmealessandro <129402291+itsmealessandro@users.noreply.github.com>
2025-06-16 17:15:23 +0200

    setup example trails_provider

---
+++ provider-trails/app/provider-trails/src/test/java/com/example/provider_trails/ProviderTrailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provider_trails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderTrailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
