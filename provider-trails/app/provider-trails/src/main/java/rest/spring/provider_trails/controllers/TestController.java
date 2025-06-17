f58121e482ae2faccb0373393190b919854d49c4
itsmealessandro <129402291+itsmealessandro@users.noreply.github.com>
2025-06-17 17:55:10 +0200

    swagger API documentation done

--- provider-trails/app/provider-trails/src/main/java/rest/spring/provider_trails/controllers/TestController.java
+++ provider-trails/app/provider-trails/src/main/java/rest/spring/provider_trails/controllers/TestController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import rest.spring.provider_trails.model.Trail;
 import rest.spring.provider_trails.services.TrialService;
 
@@ -23,6 +25,11 @@ public class TestController {
   }
 
   @GetMapping(value = "/trails", produces = MediaType.APPLICATION_JSON_VALUE)
+  // INFO: This is only for the swagger documentation
+  @Operation(summary = "Get all trails", description = "Returns a list of available trails in Abruzzo", responses = {
+      @ApiResponse(responseCode = "200", description = "Successful response with list of trails"),
+      @ApiResponse(responseCode = "404", description = "No trails found")
+  })
   public ResponseEntity<Set<Trail>> getAllTrails() {
     Set<Trail> trails = null;
     trails = trialService.getAllTrails();
