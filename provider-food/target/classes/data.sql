0c9891d103b89fc8773671206bc579c4095caf04
Ago95Dev <agostino.dagostino.developer@gmail.com>
2025-06-24 16:56:38 +0200

    Aggiornamenti vari

--- provider-food/target/classes/data.sql
+++ provider-food/target/classes/data.sql
@@ -1,17 +1,6 @@
-INSERT INTO restaurant(id, name, location, rating, cuisine_type) 
+INSERT INTO restaurant(id, name, location, rating, cuisine_type, specialties) 
 VALUES
-('r1', 'Taverna Ducale', 'Sulmona', 4.7, 'TRADITIONAL'),
-('r2', 'Ristorante del Lago', 'Scanno', 4.5, 'SEAFOOD'),
-('r3', 'La Grotta dei Sapori', 'Roccaraso', 4.8, 'MOUNTAIN'),
-('r4', 'Il Calesse', 'Pescocostanzo', 4.6, 'TRADITIONAL');
-
-INSERT INTO restaurant_specialties (restaurant_id, specialty)
-VALUES
-('r1', 'Arrosticini'),
-('r1', 'Maccheroni alla chitarra'),
-('r2', 'Trota'),
-('r2', 'Coregone'),
-('r3', 'Polenta'),
-('r3', 'Cinghiale'),
-('r4', 'Agnello'),
-('r4', 'Formaggi locali');
+('r1', 'Taverna Ducale', 'Sulmona', 4.7, 'TRADITIONAL', 'Arrosticini,Maccheroni alla chitarra'),
+('r2', 'Ristorante del Lago', 'Scanno', 4.5, 'SEAFOOD', 'Trota,Coregone'),
+('r3', 'La Grotta dei Sapori', 'Roccaraso', 4.8, 'MOUNTAIN', 'Polenta,Cinghiale'),
+('r4', 'Il Calesse', 'Pescocostanzo', 4.6, 'TRADITIONAL', 'Agnello,Formaggi locali');
