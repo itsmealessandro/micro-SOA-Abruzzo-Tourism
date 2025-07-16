315416f9e8bd8a46c37b8d104b3eb0235bb69b03
itsmealessandro <129402291+itsmealessandro@users.noreply.github.com>
2025-07-16 11:45:10 +0200

    weatherAdaptability field on trail

---
+++ provider-trails/app/provider-trails/src/main/java/rest/spring/provider_trails/model/WeatherAdaptability.java
@@ -0,0 +1,9 @@
+package rest.spring.provider_trails.model;
+
+public enum WeatherAdaptability {
+  FANGOSO, // bad for rain
+  ESPOSTO, // good for rain and snow
+  COPERTO, // good for rain and snow
+  INVERNALE // good for snow
+
+}
